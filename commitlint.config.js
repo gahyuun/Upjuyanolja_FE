95f13c8d24cb66c21afd25e66b102ddf4b7118c9
89882 <pgh123166@naver.com>
2024-01-02 16:51:32 +0900

    Chore: commit lint convention 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,29 +1,24 @@
-// module.exports = {
-//   extends: ['@commitlint/config-conventional'],
-//   rules: {
-//     'type-enum': [
-//       2,
-//       'always',
-//       [
-//         'Feat',
-//         'Fix',
-//         'Style',
-//         'Docs',
-//         'Refactor',
-//         'Rename',
-//         'Delete',
-//         'Chore',
-//         'Test',
-//         'Build',
-//         'Setting',
-//       ],
-//     ],
-//   },
-// };
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['Setting']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'Setting',
+        'Feat',
+        'Fix',
+        'Style',
+        'Docs',
+        'Refactor',
+        'Rename',
+        'Delete',
+        'Chore',
+        'Test',
+        'Build',
+        'Setting',
+      ],
+    ],
     'type-case': [2, 'always', 'pascal-case'],
   },
 };
