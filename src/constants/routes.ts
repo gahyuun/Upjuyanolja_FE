6f53675be0cb2ecd8a88d880eddf6cfdea7e46a9
89882 <pgh123166@naver.com>
2024-01-02 18:35:01 +0900

    Chore: router 상수 분리

---
+++ src/constants/routes.ts
@@ -0,0 +1,16 @@
+export const ROUTES = {
+  SIGNIN: '/signin',
+  SIGNIN_AGREEMENT: '/signin/agreement',
+  SIGNUP: '/signup',
+  SIGNUP_SUCCESS: 'signup/success',
+  POINT_DETAIL: '/point-detail',
+  INIT: '/init',
+  INIT_ACCOMMODATION_REGISTRATION: '/init/accommodation-registration',
+  INIT_ROOM_REGISTRATION: '/init/room-registration',
+  INIT_INFO_CONFIRMATION: '/init/info-confirmation',
+  COUPON: '/coupon',
+  MAIN: '/',
+  ROOM: '/room',
+  ROOM_REGISTRATION: '/room/room-registration',
+  ROOM_UPDATE: '/room/room-update',
+};
